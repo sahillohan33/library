f4d9a52b53ce8ad0a771ebe5ff5d07a5d4c274f8
Sahil <92289268+sahillohan33@users.noreply.github.com>
2023-11-06 12:17:42 +0530

    Add files via upload

---
+++ admin/includes/footer.php
@@ -0,0 +1,12 @@
+   <section class="footer-section">
+       <div class="container">
+           <div class="row">
+               <div class="col-md-12">
+                   &copy; <?php echo date('Y');?> Online Library Management System |<a
+                       href="https://www.linkedin.com/in/sahil-lohan-594b2a1b7/?originalSubdomain=ins" target="_blank">
+                       Developed by : Sahil Lohan</a>
+               </div>
+
+           </div>
+       </div>
+   </section>
